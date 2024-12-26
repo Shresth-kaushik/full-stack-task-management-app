47bbe49273d625f7b37d1dc25ce9970fcef96d8f
Shresth Kaushik <107255313+Shresth-kaushik@users.noreply.github.com>
2024-12-26 11:31:46 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
+const path = require('path')
+
+
 const app = express();
 const PORT = process.env.PORT || 5002;
 
@@ -13,6 +16,9 @@ app.use(cors({
 }));
 
 app.use(express.json());
+
+const _dirname = path.resolve()
+
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
@@ -27,6 +33,11 @@ const taskRoutes = require('./routes/tasks');
 
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
+
+app.use(express.static(path.join(_dirname , "/frontend/build")))
+app.use('*' , (req, res)=>{
+  res.sendFile(path.resolve(_dirname , "frontend" , "build" , "index.html"))
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
